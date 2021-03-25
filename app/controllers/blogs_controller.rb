890e2ba97c6b529f873cb529956e77964834e4e0
Ohta <OhtaPano@vivaldi.net>
2021-03-26 02:12:44 +0900

    commentにpagination追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -17,6 +17,8 @@ class BlogsController < ApplicationController
 
   def show
     @blog =Blog.find(params[:id])
+    @comments = @blog.comments.order("id DESC")
+    @comments = @blog.comments.page(params[:page]).per(70)
     impressionist(@blog, nil, unique: [:ip_address])
   end
 
