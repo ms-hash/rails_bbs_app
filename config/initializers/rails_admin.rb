ca93e9adf97858ab1e87a0275687e532f0cacb84
Ohta <OhtaPano@vivaldi.net>
2021-03-27 23:47:56 +0900

    admin ユーザー&パスワード設定

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,5 +1,10 @@
 RailsAdmin.config do |config|
 
+  config.authenticate_with do 
+    authenticate_or_request_with_http_basic('Site Message') do |username, password|
+      username == 'OhtaPanopticon' && password == 'DeDe1078KeiSatoh'
+    end
+  end
   ### Popular gems integration
 
   ## == Devise ==
